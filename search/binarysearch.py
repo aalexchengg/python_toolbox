87ea25b83a6f0ab7f28b83d02839923db8804d1d
Alex Cheng <abcheng@Alexs-MacBook-Air-9.local>
2024-12-24 13:19:19 +0800

    binary search my way

--- search/binarysearch.py
+++ search/binarysearch.py
@@ -2,21 +2,27 @@ from search_test import test_first, test_last
 
 # returns first occurence in the array
 def binarysearch_first(arr, target):
-    low, high = 0, len(arr)-1
-    first = -1
-
-    while low <= high:
-        mid = (low+high)//2
-
-        if arr[mid] == target:
-            first = mid
-            high = mid - 1
-
-        elif arr[mid] > target:
-            high = mid - 1
+    lo = 0
+    hi = len(arr)
+    if (arr[lo]) == target:
+        return lo
+    while (lo < hi):
+        mid = lo + (hi - lo)//2
+        if arr[mid] >= target:
+            hi = mid
         else:
-            low = mid + 1
-    return first
+            lo = mid + 1
+    # case 1: target too high, so out of bounds
+    if lo == len(arr):
+        return -1
+    # case 2: we're at the correct spot
+    if arr[lo] == target:
+        return lo
+    # case 3: we're at the spot just before
+    if arr[lo + 1] == target:
+        return lo + 1
+    # case 4: dne in array.
+    return -1
 
 # returns last occurence in the array
 def binarysearch_last(arr, target):
